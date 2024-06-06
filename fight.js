a313e8bf0d3738587670c95ae5ccd2277b58dff7
niteshkumar257 <niteshreddy257@gmail.com>
2024-06-06 20:40:48 +0530

    fight logic

---
+++ fight.js
@@ -0,0 +1,32 @@
+function fight(player1,player2)
+{
+    let attacker=player1.health<=player2.health? player1 : player2;
+    let defender=attacker===player1 ? player2 : player1;
+
+    while(player1.health>0 && player2.health>0)
+        {
+            console.log(`\n${attacker.name} attacks ${defender.name}`);
+            const attackRoll=attacker.rollDie();
+            const attackDamage=attacker.calculateDamage(attackRoll);
+
+            const defenseRoll=defender.rollDie();
+            const defenseValue=defender.calculateDefense(defenseRoll);
+
+            const damage=Math.max(0,attackDamage-defenseValue);
+            defender.health=Math.max(0,defender.health-damage);
+            console.log(`${attacker.name} rolls ${attackRoll}, attack damage: ${attackDamage}`);
+            console.log(`${defender.name} rolls ${defenseRoll}, defense value: ${defenseValue}`);
+            console.log(`${defender.name} takes ${damage} damage, health now: ${defender.health}`);
+
+            [attacker,defender]=[defender,attacker];
+            // swap the values
+
+
+        }
+
+        const winner = player1.health > 0 ? player1 : player2;
+        console.log(`\n${winner.name} wins the match!`);
+
+}
+
+module.exports=fight;
